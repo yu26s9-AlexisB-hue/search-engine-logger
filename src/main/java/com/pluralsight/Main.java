d4577dff109bdd76e59a519357555a58c4fa95f9
Alexis Barrett <happynessb09@gmail.com>
2026-04-21 16:12:36 -0400

    setting up my code

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+import java.util.Scanner;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        //Prompting the user
+        System.out.print("Enter a search term (X to exit): ");
+        
+
+    }
+}
