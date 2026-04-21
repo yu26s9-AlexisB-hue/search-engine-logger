81b0985461a14fd2c3006837326942fbf924a359
Alexis Barrett <happynessb09@gmail.com>
2026-04-21 16:33:11 -0400

    Update

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -5,10 +5,27 @@ import java.io.*;
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        String input;
 
         //Prompting the user
         System.out.print("Enter a search term (X to exit): ");
-        
+        input = scanner.nextLine();
 
+
+
+
+
+    }
+
+    private void  search(){
+        System.out.print("Searching");
+    }
+
+    private void  launch(){
+        System.out.print("Searching");
+    }
+
+    private void  exit(){
+        System.out.print("Searching");
     }
 }
